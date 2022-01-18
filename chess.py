279ed84e57b36eee06a710ba378916cff1e6a03e
Jack Jiang <jackjiang871@gmail.com>
2022-01-17 18:19:00 -0800

    Chess in Python

---
+++ chess.py
@@ -0,0 +1,14 @@
+'''
+
+'''
+def move(move_from, move_to, board):
+    pass
+
+board = [['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
+         ['♟' for _ in range(8)],
+         [' ' for _ in range(8)],
+         [' ' for _ in range(8)],
+         [' ' for _ in range(8)],
+         [' ' for _ in range(8)],
+         ['♙' for _ in range(8)],
+         ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']]
