4805eb244b76c53073e65b222703370e1bb73cd4
Jack Jiang <jackjiang871@gmail.com>
2022-03-13 13:52:22 -0700

    change route

--- chess_controller.py
+++ chess_controller.py
@@ -5,8 +5,7 @@ import chess
 
 bp = Blueprint('chess_controller', __name__)
 
-@bp.route('/next-board', methods=(['GET']))
+@bp.route('/', methods=(['GET']))
 def index():
     # chess.get_updated_board_if_is_valid_move()
-    request.args.get("r1")
     return render_template('base.html')
