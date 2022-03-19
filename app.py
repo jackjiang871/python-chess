82c2128118b7073926bb69dfa3fd971ad3a34772
Jack Jiang <jackjiang871@gmail.com>
2022-03-19 00:02:59 -0700

    added CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     import chess_controller
     app.register_blueprint(chess_controller.bp)
+    CORS(app)
     return app
