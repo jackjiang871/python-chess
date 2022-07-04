b0d1ec62261335df73d74ffabe83bad82c8badf9
Jack Jiang <jackjiang871@gmail.com>
2022-07-03 18:35:06 -0700

    Import the routes

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from flask import Blueprint
 
 bp = Blueprint('chess_controller', __name__)
 
+import routes.create_user
+import routes.get_players
+import routes.health_check
+import routes.update_board
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp)
